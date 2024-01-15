cd8281ff7ca489c95d07cebec9cdcd82535045f1
dogoshi <info@dogoshi.io>
2024-01-15 11:29:20 +0600

    accept chages

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-=======
 import { sanity_projext_id } from "@/constant/sanity";
->>>>>>> 51ae5f1 (update)
+
 import { createClient } from "next-sanity";
 
 export const client = createClient({
   apiVersion: "2024-01-11",
   dataset: "production",
-<<<<<<< HEAD
-  projectId: "gnv18c8c",
-=======
+
   projectId: sanity_projext_id,
->>>>>>> 51ae5f1 (update)
+
   useCdn: false,
 });
--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-=======
 import { sanity_projext_id } from "@/constant/sanity";
->>>>>>> 51ae5f1 (update)
 import createImageUrlBuilder from "@sanity/image-url";
 import type { Image } from "sanity";
 
 const imageBuilder = createImageUrlBuilder({
-<<<<<<< HEAD
-  projectId: "gnv18c8c" || "",
-=======
   projectId: sanity_projext_id || "",
->>>>>>> 51ae5f1 (update)
+
   dataset: "production" || "",
 });
 
